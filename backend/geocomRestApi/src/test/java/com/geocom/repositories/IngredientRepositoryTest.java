f8b9620ece8f1c5c00dbbd7b3d552c8217908b9f
Eduardo Pérez <eduardo.perez.2151@gmail.com>
2018-02-05 21:48:48 -0300

    Se agrega configuracion faltante de Cors y Anotacion de ActiveProfile para testing.

--- backend/geocomRestApi/src/test/java/com/geocom/repositories/IngredientRepositoryTest.java
+++ backend/geocomRestApi/src/test/java/com/geocom/repositories/IngredientRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class IngredientRepositoryTest {
